31fc1069c8fd80860264e06fb82bec2470ccb1e5
AaroKivimäki <aaro.kivimaki@gmail.com>
2025-03-25 20:07:58 +0200

    Made the basic things

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// ----- Text input and add button -----
+
+const createMindMapElement = document.createElement("div")
+const createInputElement = document.createElement("input") // Uses values
+const createButtonElement = document.createElement("button")
+
+createMindMapElement.classList = "mindMapDiv"
+createInputElement.className = "inputBox"
+createButtonElement.className = "addButton"
